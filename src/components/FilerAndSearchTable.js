ac3bd6bb349fe9df229d4c1947c9add4cf564d02
chenzehao <397938635@qq.com>
2019-11-13 17:27:32 +0800

    init commit

--- src/components/FilerAndSearchTable.js
+++ src/components/FilerAndSearchTable.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import   { Table, Button } from 'antd';
-
+import {Table, Button} from 'antd';
 
 
 export class FilerAndSearchTable extends React.Component {
     state = {
         filteredInfo: null,
         sortedInfo: null,
-        data:[]
+        data: []
     };
-    constructor(props){
+
+    constructor(props) {
         super(props);
     }
+
     componentDidMount() {
-        this.setState({data:this.props.data})
+        this.setState({data: this.props.data})
     }
 
     handleChange = (pagination, filters, sorter) => {
-        console.log('Various parameters', pagination, filters, sorter);
+        console.log('Various parameters', pagination, "filters:", filters, "sorter:", sorter);
         this.setState({
             filteredInfo: filters,
             sortedInfo: sorter,
@@ -25,7 +26,7 @@ export class FilerAndSearchTable extends React.Component {
     };
 
     clearFilters = () => {
-        this.setState({ filteredInfo: null });
+        this.setState({filteredInfo: null});
     };
 
     clearAll = () => {
@@ -43,51 +44,29 @@ export class FilerAndSearchTable extends React.Component {
             },
         });
     };
-
-    render() {
-        let { sortedInfo, filteredInfo } = this.state;
+    mapPropsToColumn = () => {
+        let {sortedInfo, filteredInfo} = this.state;
         sortedInfo = sortedInfo || {};
         filteredInfo = filteredInfo || {};
-        const columns = [
-            {
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name',
-                filters: [{ text: 'Joe', value: 'Joe' }, { text: 'Jim', value: 'Jim' }],
-                filteredValue: filteredInfo.name || null,
-                onFilter: (value, record) => record.name.includes(value),
-                sorter: (a, b) => a.name.length - b.name.length,
-                sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
-                ellipsis: true,
-            },
-            {
-                title: 'Age',
-                dataIndex: 'age',
-                key: 'age',
-                sorter: (a, b) => a.age - b.age,
-                sortOrder: sortedInfo.columnKey === 'age' && sortedInfo.order,
-                ellipsis: true,
-            },
-            {
-                title: 'Address',
-                dataIndex: 'address',
-                key: 'address',
-                filters: [{ text: 'London', value: 'London' }, { text: 'New York', value: 'New York' }],
-                filteredValue: filteredInfo.address || null,
-                onFilter: (value, record) => record.address.includes(value),
-                sorter: (a, b) => a.address.length - b.address.length,
-                sortOrder: sortedInfo.columnKey === 'address' && sortedInfo.order,
-                ellipsis: true,
-            },
-        ];
+        let column = this.props['columns'] || [];
+        return column.map(function (item) {
+            let dataIndex = item['dataIndex'];
+            item["sortOrder"] = sortedInfo.columnKey === dataIndex && sortedInfo.order;
+            item['filteredValue'] = filteredInfo[dataIndex] || null;
+            return item;
+        })
+    }
+
+    render() {
+        let {sortedInfo, filteredInfo} = this.state;
+
+        let columns = this.mapPropsToColumn();
+        console.log(columns, "columns");
         return (
             <div>
                 <div className="table-operations">
-                    <Button onClick={this.setAgeSort}>Sort age</Button>
-                    <Button onClick={this.clearFilters}>Clear filters</Button>
-                    <Button onClick={this.clearAll}>Clear filters and sorters</Button>
                 </div>
-                <Table columns={columns} dataSource={data} onChange={this.handleChange} />
+                <Table columns={columns} dataSource={this.props.data} onChange={this.handleChange}/>
             </div>
         );
     }
