2e446508a2f5549e5f81f76490fcc6c0613a8466
chenzehao <397938635@qq.com>
2019-12-20 16:56:54 +0800

    init commit

--- src/layouts/SendandReceiving.js
+++ src/layouts/SendandReceiving.js
@@ -3,15 +3,49 @@ import React from "react";
 import {CorePanel} from "../components/CorePanel";
 import {CoreSearchAndEditTable} from "../components/CoreSearchAndEditTable";
 
+const data = [];
+for (let i = 0; i < 100; i++) {
+    data.push({
+        key: i.toString(),
+        name: `Edrward ${i}`,
+        age: 32,
+        address: `London Park no. ${i}`,
+    });
+}
+
+function onSave(props) {
+    console.log('outer', props)
+}
+
+let columns = [
+    {
+        title: 'name',
+        dataIndex: 'name',
+        width: '25%',
+        editable: true,
+        canSearch: true,
+    },
+    {
+        title: 'age',
+        dataIndex: 'age',
+        width: '15%',
+        editable: true,
+    },
+    {
+        title: 'address',
+        dataIndex: 'address',
+        width: '40%',
+        editable: true,
+    },
+];
 
 export class SendAndReceiving extends React.Component {
     render() {
-        const column = []
         return (
 
             <div style={{width: "80%", margin: " 100px auto"}}>
                 <CorePanel desc={"aaaa"} title={"ttttt"}>
-                <CoreSearchAndEditTable/>
+                    <CoreSearchAndEditTable dataSource={data} onSave={onSave} columns={columns}/>
                 </CorePanel>
             </div>
 
